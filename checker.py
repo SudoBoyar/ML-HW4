bc239aacf2dfba6eb9745c3f180862f28754c20e
Alex Klein <S.Alex.Klein@gmail.com>
2018-04-11 14:03:57 -0600

    Added timing to test method and train calls

--- checker.py
+++ checker.py
@@ -1,20 +1,51 @@
 import argparse
 import os
 import sys
+from time import time
 
 from DataLoader import DataLoader
 from FingerprintIdentifier import FingerprintIdentifier
 
 
+class Timer:
+    """
+    with Timer("name"): to make timing easier
+    """
+
+    def __init__(self, name=None, output=True):
+        self.name = name
+        self.output = output
+        self.elapsed = 0.0
+
+    def __enter__(self):
+        self.start = time()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time()
+        self.elapsed = self.end - self.start
+        if self.output:
+            print(self)
+
+    def __str__(self):
+        return "{}: {:0.6f} seconds".format(self.name, self.elapsed) if self.name else "{:0.6f} seconds".format(self.elapsed)
+
+
 def test(args):
+    useTimer = args.time
     loader = DataLoader()
-    loader.load_samples()
+    with Timer('Load Unfiltered Data', useTimer):
+        loader.load_samples()
 
     identifier = FingerprintIdentifier(loader.get(start=1))
+    with Timer('SVM Train', useTimer):
+        identifier.train()
+
     test = loader.get(start=0, end=1)
     correct = 0
     incorrect = 0
     total = 0
+
     for k, vs in test.items():
         for v in vs:
             prediction = identifier.identify([v])
@@ -25,14 +56,18 @@ def test(args):
                 incorrect += 1
             total += 1
 
-    print("SVM: ", correct/total * 100, '% correct ', incorrect/total * 100, '% incorrect')
+    print("Unfiltered Data: ", correct/total * 100, '% correct ', incorrect/total * 100, '% incorrect')
 
     del loader
 
     loader = DataLoader(use_gabor=True)
-    loader.load_samples()
+    with Timer('Load Gabor Data', useTimer):
+        loader.load_samples()
 
     identifier = FingerprintIdentifier(loader.get(start=1))
+    with Timer('Train Gabor SVM', useTimer):
+        identifier.train()
+
     test = loader.get(start=0, end=1)
     correct = 0
     incorrect = 0
@@ -52,9 +87,12 @@ def test(args):
     del loader
 
     loader = DataLoader(pca_components=1000)
-    loader.load_samples()
+    with Timer('Load PCA Data', useTimer):
+        loader.load_samples()
 
     identifier = FingerprintIdentifier(loader.get(start=1))
+    with Timer('Train PCA', useTimer):
+        identifier.train()
     test = loader.get(start=0, end=1)
     correct = 0
     incorrect = 0
@@ -85,6 +123,7 @@ def main(args):
     loader.load_samples(folder=training)
 
     identifier = FingerprintIdentifier(loader.get())
+    identifier.train()
     for filename in testing:
         f = loader.load_file(filename)
         prediction = identifier.identify(f)
@@ -97,6 +136,7 @@ def main(args):
     loader.load_samples(folder=training)
 
     identifier = FingerprintIdentifier(loader.get())
+    identifier.train()
     for filename in testing:
         f = loader.load_file(filename)
         prediction = identifier.identify(f)
@@ -108,9 +148,10 @@ def main(args):
     loader = DataLoader(pca_components=1000)
     loader.load_samples(folder=training)
 
+    identifier = FingerprintIdentifier(loader.get())
+    identifier.train()
     for filename in testing:
         f = loader.load_file(filename)
-        identifier = FingerprintIdentifier(loader.get())
         prediction = identifier.identify(f)
 
         print("PCA Predicted ", prediction, ' for ', filename)
@@ -119,6 +160,7 @@ def main(args):
 def parseArgs(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('filename', nargs='+')
+    parser.add_argument('-t', '--time', action='store_true')
     return parser.parse_args(args)
 
 
